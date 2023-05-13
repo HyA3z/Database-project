dba35644cd99377ddb1decf889bdc18bbb609185
ziyang chen <98240581+HyA3z@users.noreply.github.com>
2023-05-13 15:51:03 +0300

    Update Triggers.sql

--- scripts/Triggers.sql
+++ scripts/Triggers.sql
@@ -30,3 +30,27 @@ CREATE TRIGGER update_avg_diagnosis_length
 AFTER INSERT ON MedicalRecords
 FOR EACH ROW
 EXECUTE FUNCTION update_patient_avg_diagnosis_length();
+
+
+
+-- 2)
+-- Automatically update the doctor's appointment counter when inserting a new row into the Appointments table
+
+-- Manually add the field to the Doctors table
+ALTER TABLE Doctors ADD COLUMN AppointmentCounter INT;
+
+CREATE OR REPLACE FUNCTION update_doctor_appointment_counter()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE Doctors
+    SET AppointmentCounter = AppointmentCounter + 1
+    WHERE DoctorID = NEW.DoctorID;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_appointment_counter
+AFTER INSERT ON Appointments
+FOR EACH ROW
+EXECUTE FUNCTION update_doctor_appointment_counter();
